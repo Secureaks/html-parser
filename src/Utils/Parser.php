cfd279b69289838ad10d064242742e7010d898c8
bastienFayant <bastien.fayant@ynov.com>
2026-04-20 10:36:03 +0000

    Fix reflected XSS by sanitizing user-submitted HTML with HTMLPurifier while preserving HTML rendering functionality (Fixes #1)

--- src/Utils/Parser.php
+++ src/Utils/Parser.php
@@ -4,7 +4,21 @@ namespace App\Utils;
 
 class Parser
 {
-    public static function parse() {
-        return htmlspecialchars($_POST['html'] ?? '', ENT_QUOTES | ENT_SUBSTITUTE, 'UTF-8');
+    public static function parse(): string
+    {
+        $html = $_POST['html'] ?? '';
+        if ($html === '') {
+            return '';
+        }
+
+        return self::sanitize($html);
+    }
+
+    private static function sanitize(string $html): string
+    {
+        $config = \HTMLPurifier_Config::createDefault();
+        $purifier = new \HTMLPurifier($config);
+
+        return $purifier->purify($html);
     }
 }
