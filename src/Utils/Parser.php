a54a8cde051af31a1d94a38f52b1e64095b9c73f
fr3u0l <freudlaleye@gmail.com>
2026-04-20 12:37:30 +0200

    Fix reflected XSS without losing the parsing functionality

--- src/Utils/Parser.php
+++ src/Utils/Parser.php
@@ -1,10 +1,19 @@
 <?php
 
 namespace App\Utils;
+use HTMLPurifier;
+use HTMLPurifier_Config;
 
 class Parser
 {
     public static function parse() {
-        return $_POST['html'] ?? '';
+        $html = $_POST['html'] ?? '';
+        $config = HTMLPurifier_Config::createDefault();
+        $config->set(
+            'HTML.Allowed',
+            'p,b,strong,i,em,ul,ol,li,a[href],br'
+        );
+        $purifier = new HTMLPurifier($config);
+        return $purifier->purify($html);    
     }
 }
